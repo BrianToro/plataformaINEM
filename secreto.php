0e330fcc28a37e98d07290539a62f29068565fdc
Brian Toro <50853211+BrianToro@users.noreply.github.com>
2020-04-23 12:23:35 -0500

    Eliminar archivo sobrante

--- secreto.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('includes/estudiantes.php');
-$estudiante = new Estudiantes();
-$estudiante->secreto('181039');
-$datos = $estudiante->getSecreto();
-foreach($datos as $clave => $valor){
-    echo $clave . " " . $valor . "  ";
-}
-?>
